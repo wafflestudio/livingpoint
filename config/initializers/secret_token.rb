b157613422c1453024a43386a3db8f6f38c34795
Taekmin Kim <tantara.tm@gmail.com>
2012-03-27 21:24:02 +0900

    LP start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LP::Application.config.secret_token = '8cd1c4c7b0354cb0bbbb260704086a203d39e0c46203f4c63a3294dabefbccab9eef18be49dbeda8b818771149155fb37c4f2aea568be3bb81376dcedc318028'
